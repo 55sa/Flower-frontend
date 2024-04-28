c3bba922812336c530849749c02309933057fcb2
jiahaohe <jiahhe@iu.edu>
2024-04-28 02:44:28 -0400

    index

---
+++ login.js
@@ -0,0 +1,34 @@
+
+
+async function login() {    
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let customer = {username: username, password: password}
+    localStorage.setItem('username', username);
+    let request = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(customer)
+      };
+      try {
+        let response = await fetch("http://localhost:1095" + "/signin", request);
+        if(response.status == 200) {  
+            alert("The login was successful!");
+            const token = await response.text();
+            configuration.token = token;
+            configuration.loggedIn = true;
+            localStorage.setItem('token', token);
+            location.href = "index.html";
+
+        } else {
+            console.log(`response status:${response.status}`);            
+            alert("Something went wrong!");
+        }
+      }
+      catch(error) {
+        console.log(error);        
+        alert("Something went wrong!");
+      }    
+}
